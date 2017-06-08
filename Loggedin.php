5ef659f53231da97a526f9ed0460ad338ef695dc
Oscar Hultin <oscar.hultin@live.se>
2017-06-08 10:15:02 +0200

    Gör en separat sida som man blir tagen till om man lyckas logga in

---
+++ Loggedin.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Login</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+<div class ="login">
+<div class="input">
+<h1> You are now logged in!</h1>
+</div>
+</div>
+
+</body>
+
+</html>
