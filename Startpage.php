41833f91e3c15294bac4c6fff5e960c4affe926e
Oscar Hultin <oscar.hultin@live.se>
2017-06-08 09:41:31 +0200

    Jag gör min startsida

--- Startpage.php
+++ Startpage.php
@@ -1,1 +1,21 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Login</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
 
+<body>
+
+<?php 
+//här gör jag min startsida använder POST för att skicka infon användaren skriver in.
+ ?>
+<form action="Login.php" method="POST">
+	<input type="text" name="username" placeholder="Username"><br>
+	<input type="Password" name="pw" placeholder="Password"><br>
+	<button>LOGIN</button>
+</form>
+
+</body>
+
+</html>
