71fbfaf3d611c377005544c6fcf68c342649c4ae
Oscar Hultin <oscar.hultin@live.se>
2017-06-08 09:53:08 +0200

    Skrev mina kommentarer så jag vet hur jag gjorde och vad jag gjorde

--- Connect.php
+++ Connect.php
@@ -1,7 +1,10 @@
 <?php
-
+//skapar min connection mellan programmet och databasen $conn
+//använder mig av metoden mysqli_connect
 $conn = mysqli_connect("localhost", "root", "", "login");
 
+//Skapar en if-sats att om connection fail med databasen så ska programmet dö
+//och då displayas också ett fel medelande
 if(!$conn){
 	die("Connection failed: ".mysqli_connect_error());
 }
